df04eb0e058d71398c3235c2ef075253e662957e
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2017-09-18 16:26:38 +0200

    adding linting

--- tests.js
+++ tests.js
@@ -1,45 +1,55 @@
+/* eslint func-names:0, global-require: 0, import/no-extraneous-dependencies:0 */
+
 module.exports = (hydra, describe, it) => {
-    const expect = require("chai").expect;
-    const request = require('supertest')('http://localhost:5000');
+  const expect = require('chai').expect;
+  const request = require('supertest')('http://localhost:5000');
 
-    describe('Hydra Cluster Dashboard', () => {
-        it('init', function (done) {
-            this.timeout(6000);
+  describe('Hydra Cluster Dashboard', () => {
+    it('init', function (done) {
+      this.timeout(6000);
 
-            setTimeout(() => {
-                done();
-            }, 5000);
-        });
+      setTimeout(() => {
+        done();
+      }, 5000);
+    });
 
-        it('get all services', async() => {
-            await request.get('/srvs').expect(200)
-                .then(response => {
-                    let srv = response.body.filter((e) => e.serviceName === "hydra-dashboard")[0];
-                    expect(srv.available).to.equal(true);
-                });
-        });
-
-        it('get service routes', async() => {
-            await request.get('/srvs/hydra-dashboard/routes').expect(200)
-                .then(response => {
-                    expect(response.body.includes('[GET]/srvs/:service/routes')).to.equal(true);
-                });
-        });
-
-        it('get service health', (done) => {
-            request.get('/srvs/hydra-dashboard/health').expect(200)
-                .then(response => {
-                    expect(response.body.length > 0).to.equal(true);
-                    done();
-                });
-        });
-
-        it('get nodes', (done) => {
-            request.get('/nodes').expect(200)
-                .then(response => {
-                    expect(response.body.length > 0).to.equal(true);
-                    done();
-                });
+    it('get all services', async () => {
+      await request
+        .get('/srvs')
+        .expect(200)
+        .then((response) => {
+          const srv = response.body.filter(e => e.serviceName === 'hydra-dashboard')[0];
+          expect(srv.available).to.equal(true);
         });
     });
-}
+
+    it('get service routes', async () => {
+      await request
+        .get('/srvs/hydra-dashboard/routes')
+        .expect(200)
+        .then((response) => {
+          expect(response.body.includes('[GET]/srvs/:service/routes')).to.equal(true);
+        });
+    });
+
+    it('get service health', (done) => {
+      request
+        .get('/srvs/hydra-dashboard/health')
+        .expect(200)
+        .then((response) => {
+          expect(response.body.length > 0).to.equal(true);
+          done();
+        });
+    });
+
+    it('get nodes', (done) => {
+      request
+        .get('/nodes')
+        .expect(200)
+        .then((response) => {
+          expect(response.body.length > 0).to.equal(true);
+          done();
+        });
+    });
+  });
+};
