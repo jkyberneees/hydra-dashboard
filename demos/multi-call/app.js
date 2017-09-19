9a94edf50aa0beeb3be2278e29ec8d93b7295985
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2017-09-19 12:53:41 +0200

    adding koa based demo and dev dependencies

---
+++ demos/multi-call/app.js
@@ -0,0 +1,62 @@
+/* eslint import/no-extraneous-dependencies:0, no-unused-vars:0, no-console:0 */
+const Koa = require('koa');
+const hydra = require('hydra');
+const HydraHttpPlugin = require('hydra-plugin-http').HydraHttpPlugin;
+
+hydra.use(new HydraHttpPlugin());
+
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+const PORT = process.env.PORT || 3003;
+
+const service = new Koa();
+const router = require('koa-router')();
+
+router.post('/complex', async (ctx) => {
+  const options = {
+    headers: {
+      'x-request-id': ctx.headers['x-request-id'],
+    },
+  };
+  const responses = [];
+
+  responses.push((await hydra.http.request.post('/v1/login', {}, options)).data);
+  responses.push((await hydra.http.request.get('/v1/notes', options)).data);
+  responses.push((await hydra.http.request.post('/v1/sendemail', {}, options)).data);
+
+  ctx.body = responses;
+  ctx.status = 200;
+});
+
+router.get('/_health', async (ctx) => {
+  ctx.status = 200;
+});
+
+service.use(router.routes());
+
+// starting koa server
+const server = service.listen(PORT, HOSTNAME);
+
+// integrating hydra
+const HydraServiceFactory = require('hydra-integration').HydraServiceFactory;
+
+const factory = new HydraServiceFactory({
+  hydra: {
+    serviceName: 'complex-service',
+    serviceDescription: 'Example koa service on top of Hydra',
+    serviceIP: HOSTNAME,
+    servicePort: PORT,
+    serviceType: 'koa',
+    serviceVersion: '1.0.0',
+    redis: {
+      host: '127.0.0.1',
+      port: 6379,
+      db: 15,
+    },
+  },
+});
+
+// sync express
+factory
+  .init()
+  .then(() => factory.sync(service))
+  .catch(console.error);
